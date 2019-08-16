feca0f114ab696abfd3add1a75d30ae2fc3c79bb
asharma29 <a.banwari07@gmail.com>
2019-08-17 03:05:07 +0530

    On branch master

--- ES6code/factorial.js
+++ ES6code/factorial.js
@@ -1,21 +1,9 @@
 var a = 5;
 b =1
 for (let i = 0;i<a;i++){
-    b =   b*[a-i];
-    
- console.log(b);
- 
-}
+    b =   b*(a-i);
+    }
+console.log(b);
 
 
-
-    	//We will find the factorial of this number
-        // let number = 5;
-        // let fact = 1;
-        // for(let i = 1; i <= number; i++)
-        // {
-        //     fact = fact * i;
-        // }
-    
-        //     console.log("" + fact);
-    
+    	
